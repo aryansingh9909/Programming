a7325b581b366a5d750bfb4186ac08d1f4f0f2cd
Aryan Singh <aryansingh9909@gmail.com>
2023-02-10 03:43:00 +0530

    CC-0027 p5 setup

---
+++ setup/p5/empty-example/sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+  // put setup code here
+  createCanvas(400, 400);
+}
+
+function draw() {
+  // put drawing code here
+  background(0);
+  fill(255);
+  ellipse(width/2, height/2, 24, 24);
+}
